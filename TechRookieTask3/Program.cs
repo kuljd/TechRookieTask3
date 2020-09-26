a43bf321100627d69dc214915d9ef7e70ebd556f
Margiris Jankauskas <jankauskas19@itu.edu.tr>
2020-09-26 13:30:15 +0300

    final version

--- TechRookieTask3/Program.cs
+++ TechRookieTask3/Program.cs
@@ -10,6 +10,74 @@ namespace TechRookieTask3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Highest floor which doesn't break the egg: " + FindHighestFloor(100));
+            Console.ReadKey();
+        }
+
+        public static int FindHighestFloor(int floorCount)
+        {
+            List<int> floors = FloorsWhichDontBreakTheEgg(floorCount);
+            int highestFloor = 0;
+            int eggCount = 2;
+
+            int i = 1;
+
+            int divider = 2;
+            int middleFloor = floorCount / divider;
+            while (eggCount > 1)
+            {
+                if (floors.Contains(middleFloor))
+                {
+                    divider = divider * 2;
+                    if (floorCount % divider == 0)
+                    {
+                        middleFloor = middleFloor + (floorCount / divider);
+                        Console.WriteLine(floorCount);
+                        i = middleFloor;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    eggCount--;
+                }
+            }
+
+            for (; i < floorCount + 1; i++)
+            {
+                if (eggCount > 0)
+                {
+                    if (floors.Contains(i))
+                    {
+                        highestFloor = i;
+                    }
+                    else
+                    {
+                        eggCount--;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return highestFloor;
+        }
+
+        public static List<int> FloorsWhichDontBreakTheEgg(int floorCount)
+        {
+            List<int> floors = new List<int>();
+            Random random = new Random();
+            int highestFloor = random.Next(0, floorCount + 1);
+
+            for (int i = 1; i < highestFloor + 1; i++)
+            {
+                floors.Add(i);
+            }
+            return floors;
         }
     }
 }
